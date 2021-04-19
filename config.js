9a73ac7f3164e872cad94469eb502afa32bf54af
Doom <73260146+MoneyDoom@users.noreply.github.com>
2021-04-19 09:59:31 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
-exports.TOKEN = 'Nzg1NTc3Njg2ODEzOTAwODcy.X854Ow.0mF3VYEg9XGzEt_3UjMaMHXCDxI';
+exports.TOKEN = 'your token';
 
 exports.DBL_API_KEY = '';
 
-exports.PREFIX = '.';
+exports.PREFIX = 'prefix you would like';
 
-exports.GOOGLE_API_KEY = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
+exports.GOOGLE_API_KEY = 'your api keys';
 
-exports.GENIUS_API_KEY = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
+exports.GENIUS_API_KEY = 'your api keys;
 
-exports.yandex_API = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
+exports.yandex_API = 'your api keys';
 
-exports.news_API = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
+exports.news_API = 'your api keys';
 
-exports.giphy_API = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
+exports.giphy_API = 'your api keys';
 
-exports.AME_API = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
+exports.AME_API = 'your api keys';
 
-exports.blague_API = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
+exports.blague_API = 'your api keys';
