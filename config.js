e275159c1e0bdbb5e145b776852aba71ec29010e
Doom <73260146+MoneyDoom@users.noreply.github.com>
2021-04-19 09:58:06 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+exports.TOKEN = 'Nzg1NTc3Njg2ODEzOTAwODcy.X854Ow.0mF3VYEg9XGzEt_3UjMaMHXCDxI';
+
+exports.DBL_API_KEY = '';
+
+exports.PREFIX = '.';
+
+exports.GOOGLE_API_KEY = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
+
+exports.GENIUS_API_KEY = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
+
+exports.yandex_API = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
+
+exports.news_API = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
+
+exports.giphy_API = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
+
+exports.AME_API = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
+
+exports.blague_API = 'AIzaSyDijoKElY1IzX66GT2rJccXkJs3BNR-FJg';
